206af2b22fabb94397e8064e1e92855ddb86a5f3
conta16 <luca.contalbo@gmail.com>
2021-12-23 17:00:24 +0100

    deleted debug prints

--- z3/SATpacking.py
+++ z3/SATpacking.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 	if (len(sys.argv) < 2):
 		raise ArgumentNumberException()
 	s = solver(sys.argv[1])
-	s.debug()
+	#s.debug()
 	s.constraints()
 	s.solve()
-	s.debug()
+	#s.debug()
